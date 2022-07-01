54db13abf2eb1d2c5bcb113b22c738c04d721db3
camcam3658 <gamegame1316@gmail.com>
2022-07-02 00:11:59 +0900

    postを修正

--- src/app/controllers/posts_controller.rb
+++ src/app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def new
     @post = Post.new
@@ -27,7 +28,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    Post.destroy
+    @post.destroy
     flash[:success] = "楽曲を削除しました"
     redirect_to request.referrer || root_url
   end
@@ -35,6 +36,11 @@ class PostsController < ApplicationController
   private
     def post_params
       params.require(:post).permit(:caption,:song).merge(user_id: current_user.id)
+    end
+
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
     end
 
 end
--- src/app/models/post.rb
+++ src/app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :song
   default_scope -> { order(created_at: :desc) }
+  validates :caption, presence: true
 end
